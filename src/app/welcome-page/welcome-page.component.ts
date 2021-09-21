03e41194c2a56b5ae1aa4d81ea04b19bff4eb5bf
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-09-21 12:18:50 -0400

    Cleaned up code

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({

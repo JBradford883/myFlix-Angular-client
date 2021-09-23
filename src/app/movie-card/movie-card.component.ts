01d59f6d241ebe7783a1aed8bea599014c08dc5a
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-09-23 10:25:33 -0400

    Added logic to open the genre, director, and synopsis views

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { GenreViewComponent } from '../genre-view/genre-view.component';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { DirectorViewComponent } from '../director-view/director-view.component';
+import { SynopsisViewComponent } from '../synopsis-view/synopsis-view.component';
+
+//const user = localStorage.getItem('username');
 
 @Component({
   selector: 'app-movie-card',
@@ -8,7 +15,9 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 })
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,) { }
 
   ngOnInit(): void {
     this.getMovies();
@@ -19,6 +28,43 @@ export class MovieCardComponent implements OnInit {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  openGenre(name: string,
+    description: string,
+  ): void {
+    this.dialog.open(GenreViewComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: '500px'
+    });
+  }
+
+  openDirector(name: string, bio: string, birth: string, death: string
+  ): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: {
+        Name: name,
+        Bio: bio,
+        Birth: birth,
+        Death: death
+      },
+      width: '500px'
+    });
+  }
+
+  openSynopsis(title: string, imagePath: any, description: string
+  ): void {
+    this.dialog.open(SynopsisViewComponent, {
+      data: {
+        Title: title,
+        ImagePath: imagePath,
+        Description: description,
+      },
+      width: '500px'
     });
   }
 
